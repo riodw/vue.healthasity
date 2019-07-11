ea4434062d12118ccb5dad0bcd7d940e67611cc7
riow <rio@medtricslab.com>
2019-07-10 23:19:32 -0400

    Axios and Google cloud working;

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
+'use strict'
+
 import Vue from 'vue'
 import App from './App.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
